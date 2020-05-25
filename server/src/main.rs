4063162701fe2de0b41be8afab07805752313d35
Philippe Vinchon <p.vinchon@gmail.com>
2020-05-25 10:58:10 +0100

    Fix static files path during development (#11)
    * Fix static files path during development
    
    * Does this fix rustfmt
    
    * Fix issue
    
    * Remove formatting for now

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,9 +1,17 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
+use rocket::config::Environment;
 use rocket_contrib::serve::StaticFiles;
+
+fn static_files_folder() -> String {
+    (match Environment::active() {
+        Ok(Environment::Production) => "public/",
+        _ => "../client/dist/",
+    }).to_string()
+}
 
 fn main() {
     rocket::ignite()
-        .mount("/", StaticFiles::from("public/"))
+        .mount("/", StaticFiles::from(static_files_folder()))
         .launch();
 }
